8ead2d74d030ad849672430336078e4194f3f712
Foodzya <afodzya@gmail.com>
2022-04-12 00:24:48 +0300

    Data script for product db modified for code-first approach

--- dataForProductDb.sql
+++ dataForProductDb.sql
@@ -1,8 +1,8 @@
-INSERT INTO user_role (name) VALUES 
+INSERT INTO roles (name) VALUES 
 	('Admin'),
 	('Customer');
 	
-INSERT INTO brand (name, foundationYear) VALUES 
+INSERT INTO brands (name, foundationyear) VALUES 
 	('Nike', 1965),
 	('Adidas', 1949),
 	('Puma', 1948),
@@ -14,7 +14,7 @@ INSERT INTO brand (name, foundationYear) VALUES
 	('Calvin Klein', 1968),
 	('Gucci', 1921);
 	
-INSERT INTO product_category (name, parent_id) VALUES 
+INSERT INTO productcategories (name, parentcategoryid) VALUES 
 	('Shoes', NULL),
 	('Sneakers', 1),
 	('Sandals', 1),
@@ -28,7 +28,7 @@ INSERT INTO product_category (name, parent_id) VALUES
 	('Jeans', 9),
 	('Shorts', 9);
 	
-INSERT INTO product (name, price, quantity, description, image, category_id, brand_id) VALUES
+INSERT INTO products (name, price, quantity, description, image, productcategoryid, brandid) VALUES
 	('Nike Cortez', 75, 10, 'Classic Nike Cortez, everyday sneakers', 'https://images.asos-media.com/products/belo-chernye-kozhanye-krossovki-nike-cortez/11115384-1-whiteblack?$n_640w$&wid=513&fit=constrain', 2, 1),
 	('Adidas Superstar', 41, 5, 'The Adidas Superstar is the brand’s magnum opus', 'https://media.gq-magazine.co.uk/photos/621e40469c02050e132ba72c/master/w_1600%2Cc_limit/Adidas_0008_Superstar.jpg', 2, 2),
 	('Puma Shibui Cat Slide Sandal', 40, 10, 'Smooth finish and lightweight design', 'https://images.urbndata.com/is/image/UrbanOutfitters/64930076_030_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=640', 3, 3),
@@ -38,7 +38,7 @@ INSERT INTO product (name, price, quantity, description, image, category_id, bra
 	('HOODED RIB SWEATSHIRT', 30, 10, 'Cropped hooded sweatshirt with long sleeves. Side vents at hem. Rib trim.', 'https://static.zara.net/photos///2021/I/0/1/p/6050/891/511/2/w/850/6050891511_6_1_1.jpg?ts=1637601497447', 6, 5),
 	('WHERES WALDO? ®STRIPED T-SHIRT', 35, 5, 'Round neck top with short sleeves. Where’s Waldo?', 'https://static.zara.net/photos///2022/V/0/1/p/4644/323/060/2/w/850/4644323060_6_1_1.jpg?ts=1647597570415', 8, 5);
 
-INSERT INTO user_account (firstName, lastName, email, phoneNumber, role_id) VALUES
+INSERT INTO users (firstname, lastname, email, phonenumber, roleid) VALUES
 	('Melissa', 'Johnson', 'MelissaBJohnson@rhyta.com', '773-381-9604', 2),
 	('Lisa', 'Hall', 'LisaKHall@rhyta.com', '917-243-7722', 2),
 	('Jason', 'Chaplin', 'JasonCChaplin@armyspy.com', '949-733-7814', 2),
@@ -46,11 +46,11 @@ INSERT INTO user_account (firstName, lastName, email, phoneNumber, role_id) VALU
 	('James', 'Hein', 'adminjames@spy.com', '339', 1);
 
 
-INSERT INTO section (name) VALUES
+INSERT INTO sections (name) VALUES
 	('Man'),
 	('Woman');
 
-INSERT INTO product_category_section (section_id, category_id) VALUES
+INSERT INTO productcategorysections (sectionid, categoryid) VALUES
 	(1, 2),
 	(2, 2),
 	(1, 3),
@@ -62,21 +62,21 @@ INSERT INTO product_category_section (section_id, category_id) VALUES
 	(1, 12),
 	(2, 12);
 
-INSERT INTO customer_order (modifiedDate, status, user_id) VALUES 
+INSERT INTO orders (modifieddate, status, userid) VALUES 
 	('2022-03-29 15:35:00', 'Completed', 1),
 	('2022-01-11 09:00:00', 'In Delivery', 4),
 	('2021-12-20 13:24:00', 'Completed', 2),
 	('2022-04-20 11:35:50', 'Completed', 3),
 	('2020-07-19 12:31:09', 'Completed', 1);
 
-INSERT INTO product_customer_order (quantity, price, product_id, order_id) VALUES
+INSERT INTO productorders (quantity, price, productid, orderid) VALUES
 	(3, 270, 6, 3),
 	(1, 90, 6, 4),
 	(3, 57, 4, 1),
 	(1, 41, 2, 2),
 	(1, 75, 1, 5);
 
-INSERT INTO review (rating, comment, user_id, product_id) VALUES 
+INSERT INTO reviews (rating, comment, userid, productid) VALUES 
 	(5, 'Top sneakers! Highly recommend!', 1, 6),
 	(2, 'Low quality shoes, idk why everybody likes it', 2, 2),
 	(4, 'Good t-shirt, 2 months already and no probs', 1, 8),
